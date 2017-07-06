d38c2fec5b9fd05b8ac7041ecdbf8d155fb1b631
Patrick Luck <stonesoup@Patricks-MacBook-Pro.local>
2017-07-05 20:56:23 -0400

    Hibernate Spring Security

---
+++ src/main/kotlin/com/stonesoupprorgramming/hibernatelogin/HibernateLoginApplication.kt
@@ -0,0 +1,149 @@
+package com.stonesoupprorgramming.hibernatelogin
+
+import org.hibernate.SessionFactory
+import org.hibernate.annotations.FetchMode
+import org.hibernate.annotations.FetchProfile
+import org.hibernate.annotations.FetchProfiles
+import org.hibernate.criterion.Restrictions
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import javax.persistence.*
+import javax.transaction.Transactional
+
+@SpringBootApplication
+class HibernateLoginApplication
+
+@Entity
+data class Roles(@field: Id @field: GeneratedValue var id : Int = 0,
+                 @field: ManyToOne(targetEntity = SiteUser::class) var user : SiteUser,
+                 var role: String = "")
+
+@FetchProfiles(
+        FetchProfile(name = "default",
+                fetchOverrides = arrayOf(
+                        FetchProfile.FetchOverride(entity = SiteUser::class, association = "roles", mode = FetchMode.JOIN)))
+)
+@Entity
+data class SiteUser (@field: Id @field: GeneratedValue var id : Int = 0,
+                     var userName: String = "",
+                     var password: String = "",
+                     var enabled : Boolean = true,
+                     var accountNonExpired: Boolean = true,
+                     var credentialsNonExpired: Boolean = true,
+                     var accountNonLocked : Boolean = true,
+                     @field: OneToMany(targetEntity = Roles::class) var roles: MutableSet<Roles> = mutableSetOf()){
+
+    fun toUser() : User {
+        val authorities = mutableSetOf<GrantedAuthority>()
+        roles.forEach { authorities.add(SimpleGrantedAuthority(it.role)) }
+        return User(userName, password,enabled, accountNonExpired, credentialsNonExpired, accountNonLocked,authorities);
+    }
+}
+
+@Configuration
+class DataConfig {
+    @Bean
+    fun sessionFactory(@Autowired entityManagerFactory: EntityManagerFactory) :
+            SessionFactory = entityManagerFactory.unwrap(SessionFactory::class.java)
+}
+
+@Configuration
+class SecurityConfig(@Autowired private val userService : UserService) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userService).passwordEncoder(BCryptPasswordEncoder())
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http.formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/display").authenticated()
+                .anyRequest().permitAll()
+
+    }
+}
+
+@Transactional
+@Service
+class UserService(@Autowired private val userRepository: UserRepository) : UserDetailsService {
+
+    override fun loadUserByUsername(userName: String): UserDetails  = userRepository.loadByUsername(userName).toUser()
+
+    fun saveOrUpdate(user : SiteUser){
+        user.password = BCryptPasswordEncoder().encode(user.password)
+        userRepository.saveOrUpdate(user)
+    }
+
+    fun allUsers() = userRepository.allUsers()
+}
+
+@Repository
+class UserRepository(@Autowired private val sessionFactory: SessionFactory){
+
+    fun saveOrUpdate(user: SiteUser){
+        sessionFactory.currentSession.saveOrUpdate(user)
+    }
+
+    fun loadByUsername(userName: String) : SiteUser =
+            sessionFactory.currentSession.createCriteria(SiteUser::class.java, "su")
+                    .add(Restrictions.eq("su.userName", userName)).uniqueResult() as SiteUser
+
+    fun allUsers(profile : String = "default") : List<SiteUser> {
+        val session = sessionFactory.currentSession
+        session.enableFetchProfile(profile)
+        return session.createCriteria(SiteUser::class.java).list() as List<SiteUser>
+    }
+}
+
+@Controller
+@RequestMapping("/register")
+class RegisterController(@Autowired private val userService: UserService){
+
+    @RequestMapping(method = arrayOf(RequestMethod.GET))
+    fun doGet(model: Model) : String{
+        model.addAttribute("user", SiteUser())
+        return "register"
+    }
+
+    @RequestMapping(method = arrayOf(RequestMethod.POST))
+    fun doPost(siteUser: SiteUser) : String{
+        userService.saveOrUpdate(siteUser)
+        return "redirect:/display"
+    }
+}
+
+@Controller
+@RequestMapping("/display")
+class UserDisplay(@Autowired private val userService: UserService){
+
+    @RequestMapping(method = arrayOf(RequestMethod.GET))
+    fun doGet(model: Model) : String{
+        model.addAttribute("users", userService.allUsers())
+        return "display"
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(HibernateLoginApplication::class.java, *args)
+}
